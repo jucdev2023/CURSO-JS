84086bac61e8c4dd8dda48774977931591998403
Juliana Silva <jujucdev2525@outlook.com>
2023-05-20 07:44:12 -0300

    regras para constantes

--- aula7/index.js
+++ aula7/index.js
@@ -1,11 +1,11 @@
 
-//não podemos criar variaveis com palavras reservada
+//não podemos criar constantes com palavras reservada
 // let let nao pode
 // let if nao pode
-//variaveis precisam ter nomes significativos 
-//nao pode começar o nome de uma variavel com um numero
+//constantes precisam ter nomes significativos 
+//nao pode começar o nome de uma constantes com um numero
 // não pode conter espaços ou traços
 //utilizamos o camelCase ex: let nomeCompletoDoCliente
 //case-sensitive significa que letra maiuscula e letra minuscula faz difença
-//nao podemos redeclarar variaveis com let
+//não pode modificar o valor de uma const
 //nao utilize VAR, UTILIZE LET
