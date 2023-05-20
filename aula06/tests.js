346d32e28beee73422d528f25b00c4673916a2e7
Juliana Silva <jujucdev2525@outlook.com>
2023-05-20 01:56:35 -0300

    coloquei comentarios sobre nomeçao de variaveis

--- aula06/tests.js
+++ aula06/tests.js
@@ -1,14 +1,12 @@
-let nome = 'joão'; //tem duas maneira. isso é uma string
-/* 
- var nome = 'joão'; é antigo utiliza var, pra se referir a uma variavel
-*/
 
-console.log(nome, 'nasceu em 1984.');
-console.log('Em 2000', nome, 'conheceu Maria.');
-console.log (nome, 'casou com maria em 2012');
-console.log ('Maria teve 1 filho com' joão, 'em 2015');
-console.log ('O filho de' nome, 'se chama Eduardo');
+//não podemos criar variaveis com palavras reservada
+// let let nao pode
+// let if nao pode
+//variaveis precisam ter nomes significativos
+//
 
-//onde tem a palavra ''nome'' vai aparecer o nome joão
-//porque nós criamos uma variavel chamada nome e ela recebe o nome joao
-
+let nome; //desclarou a variavel
+nome = 'joão'; //inicializando a variavel
+console.log (nome);
+nome = 'otavio';
+console.log (nome);
