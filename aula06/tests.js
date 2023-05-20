8ab2ddc6411d5877709342d23f3beeec23d088a6
Juliana Silva <jujucdev2525@outlook.com>
2023-05-20 01:59:45 -0300

    nomeaçao de variaveis, comit corrigido

--- aula06/tests.js
+++ aula06/tests.js
@@ -2,8 +2,8 @@
 //não podemos criar variaveis com palavras reservada
 // let let nao pode
 // let if nao pode
-//variaveis precisam ter nomes significativos
-//
+//variaveis precisam ter nomes significativos 
+//nao pode começar o nome de uma variavel com um numero
 
 let nome; //desclarou a variavel
 nome = 'joão'; //inicializando a variavel
