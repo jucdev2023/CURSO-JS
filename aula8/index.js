f7db755dfe21fae3c0da1bc8c43b0eee73388eb7
Juliana Silva <jujucdev2525@outlook.com>
2023-05-20 13:49:40 -0300

    exercicio da aula 8 e template string

---
+++ aula8/index.js
@@ -0,0 +1,27 @@
+//primeira pergunta que vc vai precisar fazer antes do exercico
+// se alguma informação vai mudar
+//se nao for mundar, use const
+//fiz usando variavel constante
+const nome = 'Luiz otavio';
+const sobrenome = 'miranda';
+const idade = 30;
+const peso = 84;
+const alturaM = 1.80;
+let indiceDeMassaCorporal;
+indiceDeMassaCorporal = peso / (alturaM*alturaM);
+let anoNascimento;
+anoNascimento = 1993-idade;
+console.log(`${nome} ${sobrenome} tem ${idade}anos, e pesa ${peso} kg`);
+console.log (`tem ${alturaM}de altura, e seu imc é: ${indiceDeMassaCorporal}`);
+console.log(`${nome} nasceu em ${anoNascimento}`);
+
+ // + 
+
+   /* 
+     exemplo: 
+     console.log(nome +' ' + sobrenome + ' tem')
+     
+
+     tem tbm o template string, onde eu posso colocar variaveis dentro de uma string usa  ${ } 
+   */
+
