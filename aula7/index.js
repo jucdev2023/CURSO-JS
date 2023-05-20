93329db5dd30596a896f20bff4e77232ef5b0e49
Juliana Silva <jujucdev2525@outlook.com>
2023-05-20 09:16:42 -0300

    aula 7

--- aula7/index.js
+++ aula7/index.js
@@ -8,4 +8,15 @@
 //utilizamos o camelCase ex: let nomeCompletoDoCliente
 //case-sensitive significa que letra maiuscula e letra minuscula faz difença
 //não pode modificar o valor de uma const
-//nao utilize VAR, UTILIZE LET
+//nao utilize const, UTILIZE LET
+
+// + - * //
+const primeiroNumero =5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3
+console.log (typeof(primeiroNumero))
+
+
+
