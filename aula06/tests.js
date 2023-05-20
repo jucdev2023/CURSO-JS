7b867327f96883942143467e76a1e111300a51ac
Juliana Silva <jujucdev2525@outlook.com>
2023-05-20 01:46:07 -0300

    variaveis

---
+++ aula06/tests.js
@@ -0,0 +1,14 @@
+let nome = 'joão'; //tem duas maneira. isso é uma string
+/* 
+ var nome = 'joão'; é antigo utiliza var, pra se referir a uma variavel
+*/
+
+console.log(nome, 'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log (nome, 'casou com maria em 2012');
+console.log ('Maria teve 1 filho com' joão, 'em 2015');
+console.log ('O filho de' nome, 'se chama Eduardo');
+
+//onde tem a palavra ''nome'' vai aparecer o nome joão
+//porque nós criamos uma variavel chamada nome e ela recebe o nome joao
+
---
+++ aula5/js/index.js
@@ -0,0 +1,4 @@
+//um comentario
+console.log("olá") // ele é so exibido no console
+console.log("esse trecho sera exibido no console do navegador")
+alert('olá mundo')
