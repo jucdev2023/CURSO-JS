0364217306d005910f43bbe3919d5f61078182f1
Juliana Silva <jujucdev2525@outlook.com>
2023-05-26 10:47:28 -0300

    diferença de var e let

---
+++ aula9/index.js
@@ -0,0 +1,7 @@
+
+//ECMAScript 2015 (ES6) é uma padronização da alinguagem javascript, DE ALGUM RECURSO QUE FOI INTRODUZIDO NA LINGUAGEM 
+var nome = 'joão';
+var nome = 'otávio';
+
+//no var eu posso redelcar uma variável quando a gente utiliza VAR
+//não crie variáveis globais! -> nome = 'joão' sem especificar se é com var ou com lest, const
