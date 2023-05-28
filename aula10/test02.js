bd94c6bfd14407a751f2b087c79db6e315934461
Juliana Silva <jujucdev2525@outlook.com>
2023-05-28 11:12:15 -0300

    comitando

---
+++ aula10/test02.js
@@ -0,0 +1,9 @@
+//operadores de atriuição
+
+let contador = 50;
+contador *= 30
+contador *= 30
+contador *= 30
+contador *= 30
+contador *= 30
+console.log (contador)
--- aula10/tests.js
+++ aula10/tests.js
@@ -1,4 +1,3 @@
 const num1 = 20
 const num2 = Number (50.80)
-const result = num1 + num2;
-console.log  (result)
+console.log  (num1 + num2)
