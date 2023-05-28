236c87d2594ca95e507cc1eb4e59eb7c98bdccd6
Juliana Silva <jujucdev2525@outlook.com>
2023-05-28 11:53:29 -0300

    aula 11, exercicio

---
+++ aula11/index.js
@@ -0,0 +1,1 @@
+// resolução que o professor fez
---
+++ aula11/script.js
@@ -0,0 +1,7 @@
+
+//exercicio
+
+const  num1 = prompt ('digite o primeiro numero:');
+const num2 = prompt ('digite o segundo numero');
+const result = Number(num1) + Number(num2);
+alert (result)
