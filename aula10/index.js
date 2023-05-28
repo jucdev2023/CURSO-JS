14b1344e4aed8eddf8a813bea0faa4cb3bb5c363
Juliana Silva <jujucdev2525@outlook.com>
2023-05-28 08:17:48 -0300

    operador de incremento

--- aula10/index.js
+++ aula10/index.js
@@ -5,6 +5,8 @@
 -> - / * **
 -> %  (não é porcentagem e sim o resto da divisão)
 */
-const num1 = '5';
-const num2 = 10;
-console.log (num1 + num2);
+let contador = 1;
+
+
+console.log (++contador);
+console.log(contador)
