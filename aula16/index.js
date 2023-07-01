8381908054fec8aff7aabbc87b86e0bc43e70945
Juliana Silva <jujucdev2525@outlook.com>
2023-06-30 21:27:29 -0300

    arrays

---
+++ aula16/index.js
@@ -0,0 +1,18 @@
+const alunos = ['João', 'Maria', 'Felipe']
+console.log (alunos)
+
+/* 
+
+ você pode imaginar uma array como sendo uma lista
+
+uma lista de coisas
+
+se eu quero criar um array, eu utilizo  [  ]
+
+eu posso colocar qualquer coisa dentro dos [  ] porem nao é uma boa prática de programação
+
+lembra que as strings são **indexadas? cada letra tem um índice**
+
+-obs: Os arrays também são indexados!
+
+*/
