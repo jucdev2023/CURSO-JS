d67569ba18f65acb6097654670b3e971cc37fff5
Juliana Silva <jujucdev2525@outlook.com>
2023-07-05 11:32:47 -0300

    finalizado a aula sobre array

--- aula16/index.js
+++ aula16/index.js
@@ -1,11 +1,28 @@
 
 
                //0       1        2
-const alunos = ['João', 'Maria', 'Felipe']
-alunos [alunos.length] = 'Caio'
-//alunos[0] = 'Marta' //substituindo um elemnto no array
+const alunos = ['João', 'Maria', 'Felipe']; // aqui também temos a 'luiza e Eduardo'
+
+console.log(typeof alunos)
+console.log (alunos instanceof Array)
+
+
+
+//alunos.push('Luiza');
+//alunos.push('Eduardo');
+//const removido = alunos.pop(); // removo elementos do final
+// alunos.unshift ('Luiza'); // adiciona no começo
+//alunos.push('Tereza'); // adiciona no fim
+//alunos.push('antonio'); // adiciona no fim
+//alunos [alunos.length] = 'Caio';
+//alunos [alunos.length] = 'mariana';
+//alunos [alunos.length] = 'Fernanda';
+//alunos[0] = 'Marta' //substituindo um elemnto no array / altera
 //alunos[3] = 'Gustavo' // adicionando mais um elemento
-console.log (alunos)
+//console.log (removido) // armazenei o elemento que eu removi dentro de uma variável
+// console.log(alunos.slice(0, 4)) //  fatiando. o ultimo elemento nao é incluido e pode ser negativo
+
+
 
 
 
