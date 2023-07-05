e15fb10113c721b948e00c8d10c9f4770c165f7b
Juliana Silva <jujucdev2525@outlook.com>
2023-07-05 10:31:34 -0300

    ainda sobre arrays, tirei as anotaçoes do codigo pra ficar limpo

--- aula16/index.js
+++ aula16/index.js
@@ -1,5 +1,34 @@
+
+
+               //0       1        2
 const alunos = ['João', 'Maria', 'Felipe']
-alunos[0] = 'Marta' //substituindo um elemnto no array
-alunos[3] = 'Gustavo' // adicionando mais um elemento
+alunos [alunos.length] = 'Caio'
+//alunos[0] = 'Marta' //substituindo um elemnto no array
+//alunos[3] = 'Gustavo' // adicionando mais um elemento
 console.log (alunos)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
